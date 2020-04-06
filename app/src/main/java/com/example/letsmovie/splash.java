4694f52d9330cb353a0a19791f51c1862c385947
Harnam kaur <kharnam11@gmail.com>
2020-04-05 22:55:17 -0400

    Adding splash activity code

--- app/src/main/java/com/example/letsmovie/splash.java
+++ app/src/main/java/com/example/letsmovie/splash.java
@@ -1,8 +1,10 @@
 package com.example.letsmovie;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+         Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash.this,First.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
     }
 }
